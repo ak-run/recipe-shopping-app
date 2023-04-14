29766c1d66cf41ac1b72e1daf2299930018d31c2
ak-run <123417735+ak-run@users.noreply.github.com>
2023-04-14 08:50:36 +0100

    adding styles to shopping list

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import ShoppingList from './ShoppingList';
 import Axios from "axios";
 
-
 function App() {
   const APP_ID = "fa099eb5";
   const APP_KEY = "2f165b5ae763968bfd4088e30dcdcff5";
@@ -15,35 +14,11 @@ function App() {
     const result = await Axios.get(url);
     console.log(result);
   };
-
-  // const [recipes, setRecipes] = useState([]);
-  // const [search, setSearch] = useState("");
-  // const [query, setQuery] = useState("chicken");
-
-  // useEffect(() => {
-  //   getRecipes();
-  // }, [query])
-  
-  // const getRecipes = async () => {
-  //   const response = await fetch
-  //         (url);
-  //   const data = await response.json();
-  //   setRecipes(data.hits);
-  // };
-
-  // const updateSearch = e => {
-  //   setSearch(e.target.value);
-  // };
-
-  // const getSearch = e => {
-  //   e.preventDefault();
-  //   setQuery(search);
-  //   setSearch("");
-  // }
   
   return (
     <>
       <div className="App">
+
         <header className="App-header">
           <Router>
             <div>
@@ -63,16 +38,7 @@ function App() {
             </div>
           </Router>
         </header>
-       {/* adding search bar
-        <div className='search-bar'> 
-      <form className="search-form" onSubmit={getSearch}  >
-        <input className="search-bar" type="text" value={search}
-             onChange={updateSearch} />
-        <button className="search-button" type="submit" >
-             Search
-        </button>
-      </form>
-    </div> */}
+        
 
       </div>
     </>
