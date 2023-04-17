cc796ce22537087bab609437cdb65b4299b5cf01
natlmartin <nat.louise.martin@gmail.com>
2023-04-17 15:54:44 +0100

    Removed blue hover on navCards
    Added :hover styles to .nav-card-link to remove blue

--- src/components/navCardGroup.jsx
+++ src/components/navCardGroup.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function scrollToTop() {
   window.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: "smooth",
   });
 }
 
@@ -16,40 +16,39 @@ class Cards extends Component {
   render() {
     return (
       <>
-      <div className="container-fluid d-flex justify-content-center align-items-center">
-        <div className="row">
-          <div className="col-md-6">
-          <Link 
-            to="/RecipeFinder" 
-            className="Link"
-            onClick={scrollToTop}
-            >
-            <Card
-              imgsrc={cooking}
-              title="Find a recipe"
-              body="Search our recipe database by ingredient to find your next dish."
-              buttontext="Search recipes"
-            />
-            </Link>
-          </div>
-          <div className="col-md-6">
-          <Link 
-            to="/ShoppingList" 
-            className="Link"
-            onClick={scrollToTop}
-            >
-            <Card
-              imgsrc={shopping}
-              title="Create a shopping list"
-              body="Enter the ingredients for your next shop to create your shopping list."
-              buttontext="Create list"
-            />
-            </Link>
+        <div className="container-fluid d-flex justify-content-center align-items-center">
+          <div className="row">
+            <div className="col-md-6">
+              <Link
+                to="/RecipeFinder"
+                className="Link nav-card-link"
+                onClick={scrollToTop}
+              >
+                <Card
+                  imgsrc={cooking}
+                  title="Find a recipe"
+                  body="Search our recipe database by ingredient to find your next dish."
+                  buttontext="Search recipes"
+                />
+              </Link>
+            </div>
+            <div className="col-md-6">
+              <Link
+                to="/ShoppingList"
+                className="Link nav-card-link"
+                onClick={scrollToTop}
+              >
+                <Card
+                  imgsrc={shopping}
+                  title="Create a shopping list"
+                  body="Enter the ingredients for your next shop to create your shopping list."
+                  buttontext="Create list"
+                />
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
       </>
-
     );
   }
 }
